1a20b3a010d8603514ce9b53f3ab084761a1632a
KiranKumar Rama <36279567+ramakirankumar@users.noreply.github.com>
2018-08-20 15:38:55 +0530

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -37,6 +37,33 @@ ynh_remove_php7 () {
 #
 # usage: ynh_add_fpm_config
 ynh_add_php7.1-fpm_config () {
+	# Configure PHP-FPM 7.1 by default
+	local fpm_config_dir="/etc/php/7.1/fpm"
+	local fpm_service="php7.1-fpm"
+	ynh_app_setting_set $app fpm_config_dir "$fpm_config_dir"
+	ynh_app_setting_set $app fpm_service "$fpm_service"
+	finalphpconf="$fpm_config_dir/pool.d/$app.conf"
+	ynh_backup_if_checksum_is_different "$finalphpconf"
+	sudo cp ../conf/php-fpm.conf "$finalphpconf"
+	ynh_replace_string "__NAMETOCHANGE__" "$app" "$finalphpconf"
+	ynh_replace_string "__FINALPATH__" "$final_path" "$finalphpconf"
+	ynh_replace_string "__USER__" "$app" "$finalphpconf"
+	sudo chown root: "$finalphpconf"
+	ynh_store_file_checksum "$finalphpconf"
+
+	if [ -e "../conf/php-fpm.ini" ]
+	then
+		finalphpini="$fpm_config_dir/conf.d/20-$app.ini"
+		ynh_backup_if_checksum_is_different "$finalphpini"
+		sudo cp ../conf/php-fpm.ini "$finalphpini"
+		sudo chown root: "$finalphpini"
+		ynh_store_file_checksum "$finalphpini"
+	fi
+	sudo systemctl reload $fpm_service
+}
+
+
+ynh_add_php7.1-fpm_config_OLD () {
 	finalphpconf="/etc/php/7.1/fpm/pool.d/$app.conf"
 	ynh_backup_if_checksum_is_different "$finalphpconf"
 	sudo cp ../conf/php-fpm.conf "$finalphpconf"
@@ -63,6 +90,19 @@ ynh_add_php7.1-fpm_config () {
 #
 # usage: ynh_remove_fpm_config
 ynh_remove_php7.1-fpm_config () {
+	local fpm_config_dir=$(ynh_app_setting_get $app fpm_config_dir)
+	local fpm_service=$(ynh_app_setting_get $app fpm_service)
+	# Assume php version 5 if not set
+	if [ -z "$fpm_config_dir" ]; then
+		fpm_config_dir="/etc/php/7.1/fpm"
+		fpm_service="php7.1-fpm"
+	fi
+	ynh_secure_remove "$fpm_config_dir/pool.d/$app.conf"
+	ynh_secure_remove "$fpm_config_dir/conf.d/20-$app.ini" 2>&1
+	sudo systemctl reload $fpm_service
+}
+
+ynh_remove_php7.1-fpm_configOLD () {
 	ynh_secure_remove "/etc/php/7.1/fpm/pool.d/$app.conf"
 	ynh_secure_remove "/etc/php/7.1/fpm/conf.d/20-$app.ini" 2>&1
 	sudo systemctl reload php7.1-fpm
